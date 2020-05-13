037fe1bc7cceabe721fc1365c0dc8a821026d825
BENSIMHON <yoan.benimhon@hetic.net>
2020-05-13 16:02:22 +0200

    nav

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yo-gif12-Projet-Js1
+## Yoan Bensimhon
+### Projet Site dynamique en JavaScript
--- src/js/app.js
+++ src/js/app.js
@@ -5,17 +5,56 @@ document.addEventListener("DOMContentLoaded", function(){
 		constructor() {
 			this.pages = [
 				{
-					h1: 'LE SPORT',
-					url: '/homepage',
-          background: '#7893c9',
-          color : 'white'
+					title: 'Home',
+					url: '#',
+					background: 'black',
+					color: 'white',
+					content: `
+						
+						<img src="../asset/img/sport.jpg" alt="girl-run-sport" >
+						
+					`,
+					dynamisme: () => {
+
+						const li = document.querySelectorAll('li')
+
+						function toUpper(element) {
+							element.innerText = element.innerText.toUpperCase()
+						}
+						li.forEach(element => {
+							toUpper(element)
+						})
+
+					}
 				},
 				{
-					h1: 'WHAT IS YOUR FAVOURITE SPORT ?',
-					url: '/quizz',
-          background: 'rgb(11, 36, 73)',
-          color : 'white'
-        
+					title: 'Sport',
+					url: '#Sport',
+					background: 'blanc',
+					color: 'white',
+					content: `
+						<div class="parallax-app">
+							<div class="parallax">
+
+							</div>
+						</div>
+					`,
+					dynamisme: () => {
+						window.addEventListener('scroll', (e) => {
+							let scrollTop = e.target.scrollingElement.scrollTop
+
+							console.log(scrollTop)
+
+							document.querySelector('.parallax').style.backgroundPosition = `center calc(50% - ${scrollTop / 2}px)`
+						})
+					}
+				},
+				{
+					title: 'Contact',
+					url: '#contact',
+					background: 'black',
+					color: 'white',
+					form: true
 				}
 			]
 		}
@@ -24,55 +63,92 @@ document.addEventListener("DOMContentLoaded", function(){
 			return this.pages.find(page => page.url == url)
 		}
 
-  }
-  class View {
+	}
 
-		constructor() {
+	class View {
+
+		constructor(pages) {
 			this.app = document.querySelector('.app')
+			this.app.innerHTML = ''
+			this.addHeader(pages)
+		}
+
+		run(dynamisme) {
+			dynamisme()
+		}
+
+		addContent(content) {
+			const contentapp = document.createElement('div')
+			contentapp.classList.add('content')
+			contentapp.innerHTML = content
+
+			this.app.appendChild(contentapp)
+		}
+
+		addHeader(pages) {
+			pages.forEach((page) => {
+				const button = document.createElement('button')
+                button.classList.add('btn')
+				button.innerText = page.title
+
+				this.app.appendChild(button)
+
+				button.addEventListener('click', () => {
+					location.hash = page.url
+				})
+			})
 		}
 
 		changeTitle(text) {
-			this.app.innerHTML = text
+			const title = document.createElement('h1')
+
+			title.innerText = text
+
+			this.app.appendChild(title)
 		}
 
 		changeBackground(color) {
 			this.app.style.background = color
-    }
-    
-    changeTextColor(color) {
+		}
+		changeColor(color) {
 			this.app.style.color = color
-    }
-    addTitle(){
-      let BigTitle = document.createElement('h1')
-      h1.classList.add('main__title')
-    }
+		}
+		addForm() {
+			let input = document.createElement('input')
+			input.setAttribute('type', 'text')
+			input.classList.add('input-text')
+
+			this.app.appendChild(input)
+		}
 
 	}
 
 	function controller() {
 		let data = new Model()
-		const currentPage = data.getPageByUrl('/homepage')
 
-		let page = new View()
+		const currentPage = data.getPageByUrl(location.hash || '#')
+
+		let page = new View(data.pages)
 		page.changeTitle(currentPage.title)
-    page.changeBackground(currentPage.background)
-    page.changeTextColor(currentPage.color)
-    if(currentPage.h1) {
-			page.addh1()
+		page.changeBackground(currentPage.background)
+page.changeColor(currentPage.color)
+		if(currentPage.content) {
+			page.addContent(currentPage.content)
+		}
+
+		if(typeof currentPage.dynamisme === 'function') {
+			page.run(currentPage.dynamisme)
+		}
+
+		if(currentPage.form) {
+			page.addForm()
 		}
 	}
+
+	window.addEventListener('hashchange', () => {
+		controller()
+	})
 
 	controller()
 
 })
-
-
-// const app = document.querySelector('.app');
-
-// function makeMessage(message) {
-//   const newMessage = document.createElement('p');
-//   const addText = document.createTextNode(message);
-//   newMessage.appendChild (addText);
-//   app.appendChild(newMessage);
-// }
-// makeMessage("HELLO WRLD");
